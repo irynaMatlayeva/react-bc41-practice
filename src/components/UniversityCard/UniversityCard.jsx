f56263361864c0d2c83b55f61b8013ff76dbea1d
SunAndrea <andriishtohryn@gmail.com>
2023-01-24 15:36:18 +0200

    added university card

---
+++ src/components/UniversityCard/UniversityCard.jsx
@@ -0,0 +1,33 @@
+import { Paper } from 'components';
+import universityIcon from '../../assets/images/mock-university.svg';
+import editButtonIcon from '../../assets/images/edit.svg';
+import deleteButtonIcon from '../../assets/images/delete.svg';
+import css from '../UniversityCard/UniversityCard.module.css';
+
+const UniversityCard = ({ name, onEdit, onDelete }) => {
+  return (
+    <Paper classes={css.container}>
+      <img className={css.universityImg} src={universityIcon} alt={name} />
+      <span className={css.university}>Университет</span>
+      <h3 className={css.universityName}>{name}</h3>
+      <div className="controls">
+        <button
+          className={css.buttonEdit}
+          type="button"
+          onClick={() => onEdit()}
+        >
+          <img src={editButtonIcon} alt="" />
+        </button>
+        <button
+          className={css.buttonDelete}
+          type="button"
+          onClick={() => onDelete()}
+        >
+          <img src={deleteButtonIcon} alt="" />
+        </button>
+      </div>
+    </Paper>
+  );
+};
+
+export default UniversityCard;
